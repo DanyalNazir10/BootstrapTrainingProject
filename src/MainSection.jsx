3a393d1cacc393f458ee183da0c3113be8137c42
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-06 06:51:34 -0800

    Adding main section

--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,5 +1,11 @@
+import SimpleSlider from "./Sliders";
+import Cards from "./Cards";
+import horizontal_slider_images from "./utils/Horizontal-Slider-Images";
+import vertical_slider_images from "./utils/Vertical-Slider-images";
+
 export default function MainSection() {
   return (
+    <>
     <main className="container-fluid">
       <div className="row">
         <div className="col-12" aria-label="breadcrumb">
@@ -16,95 +22,162 @@ export default function MainSection() {
         </div>
       </div>
 
-      <div className="row" style={{ background: "blue" }}>
+      <div className="row">
         <div className="container">
           <div className="row">
-            <div
-              className="col-lg-6"
-              style={{ background: "yellow", height: "700px" }}
-            >
-              <div className="row">
-                <div
-                  className="col-lg-4"
-                  style={{ background: "purple", height: "700px" }}
-                >
-                  <div className="vertical-slick-nav">
-                    <div>your content</div>
-                    <div>your content</div>
-                    <div>your content</div>
-                    <div>your content</div>
-                    <div>your content</div>
-                    <div>your content</div>
-                  </div>
+            <div className="col-lg-6">
+              <div className="row w-100">
+                <div className="col-lg-4 col-4 vertical-slider">
+                  <SimpleSlider
+                    style={{ margin: "50px" }}
+                    isVertical={true}
+                    Slider_images={vertical_slider_images}
+                  />
                 </div>
-                <div
-                  className="col-lg-8"
-                  style={{ background: "brown", height: "700px" }}
-                >
-                  <div className="row" style={{ background: "white" }}>
-                    AGELESS MULTI-COLLAGEN® PROTEIN POWDER
+                <div className="col-lg-8 col-6">
+                  <div className="row justify-content-center text-center">
+                    <h3 style={{color: "#33659e"}}>AGELESS MULTI-COLLAGEN® PROTEIN POWDER</h3>
                   </div>
                   <div className="row">
-                    <span class="stars">
-                    <i className="fa-solid fa-star" style={{color: "#3ed016;"}}></i>
-                    <i className="fa-solid fa-star" style={{color: "#3ed016;"}}></i>
-                    <i className="fa-solid fa-star" style={{color: "#3ed016;"}}></i>
-                    <i className="fa-solid fa-star" style={{color: "#3ed016;"}}></i>
-                    <i className="fa-solid fa-star" style={{color: "#3ed016;"}}></i>
-                    <a className="">3239 Reviews</a>
+                    <span className="stars text-center">
+                      <i
+                        className="fa-solid fa-star"
+                        style={{ color: "#3ed016" }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-star"
+                        style={{ color: "#3ed016" }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-star"
+                        style={{ color: "#3ed016" }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-star"
+                        style={{ color: "#3ed016" }}
+                      ></i>
+                      <i
+                        className="fa-solid fa-star"
+                        style={{ color: "#3ed016" }}
+                      ></i>
+                      <a style={{textDecoration:"none", color:"#33659e"}}>3239 Reviews</a>
                     </span>
                   </div>
-                  <div
-                    className="row vertical-slick-for"
-                    style={{ background: "yellow", height: "600px" }}
-                  ></div>
-                  <div className="row" style={{ background: "black" }}>
-                    <img src={"https://cdn.shopify.com/s/files/1/2451/8571/t/3/assets/free-report-image-1546015359856-1555446388183.png"} alt="Receive two free e-books"></img>
+                  <div className="row">
+                    <img
+                      className="slider-main-img"
+                      src="https://www.biotrust.com/cdn/shop/products/Unflavored-4-Delicious-Flavors_1080x.png?v=1657121683"
+                    ></img>
+                  </div>
+                  <div className="row">
+                    <img
+                      src={
+                        "https://cdn.shopify.com/s/files/1/2451/8571/t/3/assets/free-report-image-1546015359856-1555446388183.png"
+                      }
+                      alt="Receive two free e-books"
+                    ></img>
                   </div>
                 </div>
               </div>
             </div>
-            <div
-              className="col-lg-6"
-              style={{ background: "green", height: "700px" }}
-            >
+            <div className="col-lg-6">
               <div className="container">
-                <div className="row" style={{ background: "purple" }}>
-                  hi
-                  <p></p>
+                <div className="row justify-content-center text-center">
+                  <h3 style={{color: "#33659e"}}>Choose Your Package</h3>
+                  <p>Select your money-saving option below:</p>
                 </div>
-                <div className="row">
-                  <div className="col-6" style={{ background: "yellow" }}>
-                    hi
+                <div className="row w-100 mb-3">
+                  <div className="col-6">
+                    <button
+                      type="button"
+                      class="btn btn-outline btn-lg"
+                      style={{
+                        color: "#33659e",
+                        width: "100%",
+                        fontWeight: "bold",
+                        border: "2px solid #33659e",
+                      }}
+                    >
+                      One-Time Purchase
+                    </button>
                   </div>
-                  <div className="col-6" style={{ background: "orange" }}>
-                    hi
+                  <div className="col-6">
+                    <button
+                      type="button"
+                      class="btn btn-primary btn-lg"
+                      style={{ background: "#33659e", width: "100%" }}
+                    >
+                      Subscribe & Save 40%
+                    </button>
                   </div>
                 </div>
-                <div className="row">
-                  <div
-                    className="col-4"
-                    style={{ background: "red", height: "400px" }}
-                  ></div>
-                  <div
-                    className="col-4"
-                    style={{ background: "blue", height: "400px" }}
-                  ></div>
-                  <div
-                    className="col-4"
-                    style={{ background: "gray", height: "400px" }}
-                  ></div>
+                <div className="row g-0">
+                  <div className="col-4">
+                    <div
+                      class="card text-center rounded-0"
+                      style={{ border: "3px solid #33659e" }}
+                    >
+                      <div
+                        class="card-header rounded-0"
+                        style={{
+                          background: "#33659e",
+                          fontWeight: "bold",
+                          color: "white",
+                        }}
+                      >
+                        MOST POPULAR
+                      </div>
+                      <div class="card-body">
+                        <h3
+                          class="card-title"
+                          style={{ color: "#33659e", fontSize: "2rem" }}
+                        >
+                          1 PACK
+                        </h3>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                          Every Four Weeks
+                        </h6>
+                        <p class="card-text">
+                          <del>$49</del>
+                          <h3 style={{ color: "#2bb34d", fontSize: "2rem" }}>
+                            $29.40 Per Pack
+                          </h3>
+                          <h5 style={{ color: "red" }}>(Save $19.16)</h5>
+                          <h6>+ FREE SHIPPING</h6>
+                        </p>
+                        <button
+                          className="rounded"
+                          style={{
+                            width: "70%",
+                            color: "white",
+                            background: "#2bb34d",
+                            border: "none",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          CHOOSE FLAVOR
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-4 d-flex align-items-center">
+                    <Cards title={"2 PACKS"} save={"39.20"}></Cards>
+                  </div>
+                  <div className="col-4 d-flex align-items-center">
+                    <Cards title={"3 PACKS"} save={"58.80"}></Cards>
+                  </div>
                 </div>
-                <div className="row">
+                <div className="row mt-5">
                   <div className="container">
-                    <div
-                      className="row"
-                      style={{ background: "white", height: "50px" }}
-                    ></div>
-                    <div
-                      className="row"
-                      style={{ background: "orange", height: "170px" }}
-                    ></div>
+                    <div className="row text-center">
+                      <h6>PHOTOS FROM THE #biotrust COMMUNITY</h6>
+                    </div>
+                    <div className="row horizontal-slider w-100">
+                      <SimpleSlider
+                        isVertical={false}
+                        Slider_images={horizontal_slider_images}
+                      ></SimpleSlider>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -112,7 +185,35 @@ export default function MainSection() {
           </div>
         </div>
       </div>
-      <div className="btn btn-ptimary btn-lg"></div>
+      <button
+        className="btn btn-ptimary btn-lg rounded-5"
+        style={{
+          position: "fixed",
+          bottom: "30px",
+          right: "50px",
+          background: "#33659e",
+          color: "white",
+          fontWeight: "bold",
+        }}
+      >
+        <i class="fa-solid fa-comment" style={{ color: "#ffffff" }}></i> Lets
+        Chat
+      </button>
+
+      <button className="btn btn-ptimary btn-lg rounded-0"
+        style={{
+          position: "fixed",
+          top: "300px",
+          left: "0",
+          zIndex: "799",
+          background: "#33659e",
+          color: "white",
+          fontSize: "1rem", 
+          transformOrigin: "0 50%",
+          transform: "rotate(-90deg) translate(-50%, 50%)"
+        }}><i class="fa-solid fa-star fa-xs" style={{color: "#f2f2f3"}}></i> REVIEWS</button>
     </main>
+      <hr className="nav-divider m-3 " style={{border: "3px solid #33659e", opacity:"2"}}/>
+      </>
   );
 }
---
+++ src/utils/Vertical-Slider-images.js
@@ -0,0 +1,11 @@
+import slider_image1 from "../images/nav-img1.png"
+import slider_image2 from "../images/nav-img2.png"
+import slider_image3 from "../images/nav-img3.png"
+import slider_image4 from "../images/nav-img4.png"
+import slider_image5 from "../images/nav-img5.png"
+import slider_image6 from "../images/nav-img6.png"
+import slider_image7 from "../images/nav-img7.png"
+
+
+
+export default [slider_image1,slider_image2,slider_image3,slider_image4,slider_image5,slider_image6,slider_image7];
