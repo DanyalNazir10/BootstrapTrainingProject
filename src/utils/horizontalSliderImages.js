60e5c73ef29e1360c4261458b3c11b41aca4e9d8
DanyalNazir10 <danyal.nazir@mergestack.com>
2023-11-08 00:42:37 -0800

    Used Camel Case in utils

--- src/utils/horizontalSliderImages.js
+++ src/utils/horizontalSliderImages.js
@@ -1,11 +1,9 @@
-import slider_image1 from "../assets/images/horizontal-slider-img1.jpg"
-import slider_image2 from "../assets/images/horizontal-slider-img2.jpg"
-import slider_image3 from "../assets/images/horizontal-slider-img3.jpg"
-import slider_image4 from "../assets/images/horizontal-slider-img4.jpg"
-import slider_image5 from "../assets/images/horizontal-slider-img5.jpg"
-import slider_image6 from "../assets/images/horizontal-slider-img6.jpg"
-import slider_image7 from "../assets/images/horizontal-slider-img7.jpg"
+import sliderImage1 from "../assets/images/horizontal-slider-img1.jpg"
+import sliderImage2 from "../assets/images/horizontal-slider-img2.jpg"
+import sliderImage3 from "../assets/images/horizontal-slider-img3.jpg"
+import sliderImage4 from "../assets/images/horizontal-slider-img4.jpg"
+import sliderImage5 from "../assets/images/horizontal-slider-img5.jpg"
+import sliderImage6 from "../assets/images/horizontal-slider-img6.jpg"
+import sliderImage7 from "../assets/images/horizontal-slider-img7.jpg"
 
-export default [slider_image1,slider_image2,slider_image3,slider_image4,slider_image5,slider_image6,slider_image7];
-
-
+export default [sliderImage1,sliderImage2,sliderImage3,sliderImage4,sliderImage5,sliderImage6,sliderImage7];
--- src/utils/verticalSliderImages.js
+++ src/utils/verticalSliderImages.js
@@ -1,9 +1,9 @@
-import slider_image1 from "../assets/images/nav-img1.png"
-import slider_image2 from "../assets/images/nav-img2.png"
-import slider_image3 from "../assets/images/nav-img3.png"
-import slider_image4 from "../assets/images/nav-img4.png"
-import slider_image5 from "../assets/images/nav-img5.png"
-import slider_image6 from "../assets/images/nav-img6.png"
-import slider_image7 from "../assets/images/nav-img7.png"
+import sliderImage1 from "../assets/images/nav-img1.png"
+import sliderImage2 from "../assets/images/nav-img2.png"
+import sliderImage3 from "../assets/images/nav-img3.png"
+import sliderImage4 from "../assets/images/nav-img4.png"
+import sliderImage5 from "../assets/images/nav-img5.png"
+import sliderImage6 from "../assets/images/nav-img6.png"
+import sliderImage7 from "../assets/images/nav-img7.png"
 
-export default [slider_image1,slider_image2,slider_image3,slider_image4,slider_image5,slider_image6,slider_image7];
+export default [sliderImage1,sliderImage2,sliderImage3,sliderImage4,sliderImage5,sliderImage6,sliderImage7];
